1ba1af985f7c17736165bc52335c3b5e61a9cc99
Fedor Noskov <noskovfedorsvv@gmail.com>
2026-05-06 21:20:06 +0300

    add: Appointment_class.cs DateTime

--- Appointment_class.cs
+++ Appointment_class.cs
@@ -2,21 +2,22 @@ class Appointment
 {
     private int _id;
     private Doctor _doctor;
-    private string _date;
+    private DateTime _dateTime;
     private bool _isBooked;
+
+    public DateTime DateTime {get => _dateTime; set => _dateTime = value;}
+    
     public int ID {get => _id; private set => _id = value > 0 ? value : 0;}
 
     public Doctor Doctor {get => _doctor; set => _doctor = value ?? null; }
 
-    public string Date {get => _date; set => _date = string.IsNullOrEmpty(value) ? "Unknown date" : value;}
-
     public bool IsBooked {get => _isBooked; set => _isBooked = value;}
 
-    public Appointment(int id, Doctor doctor, string date, bool isBooked)
+    public Appointment(int id, Doctor doctor, DateTime dateTime, bool isBooked)
     {
         ID = id;
         Doctor = doctor;
-        Date = date;
+        DateTime = dateTime;
         this.IsBooked = isBooked;
     }
 
