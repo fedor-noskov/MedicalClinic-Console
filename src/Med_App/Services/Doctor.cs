4068a4fb18f96b55d1b3f84ae978f29ae8ded786
fedor_noskov <noskovfedorsvv@gmail.com>
2026-06-19 13:36:40 +0300

    refactor: use Guard in Doctor, Patient, Appointment

--- src/Med_App/Services/Doctor.cs
+++ src/Med_App/Services/Doctor.cs
@@ -1,13 +1,13 @@
-
+using Med_App;
 public class Doctor
 {
     private int _id;
     private string _name;
     private string _specialization;
     
-    public int ID { get => _id; private set => _id = value > 0 ? value : throw new ArgumentException("ID должен быть положительным!"); }
-    public string Name { get => _name; set => _name = string.IsNullOrEmpty(value) ? throw new ArgumentException("Имя не может быть пустым!") : value; }
-    public string Specialization { get => _specialization; set => _specialization = string.IsNullOrEmpty(value) ? throw new ArgumentException("Специализация не может быть пустой!") : value; }
+    public int ID { get => _id; private set => _id = Guard.Positive(value, "ID");}
+    public string Name { get => _name; set => _name = Guard.NotEmpty(value, "Имя"); }
+    public string Specialization { get => _specialization; set => _specialization = Guard.NotEmpty(value, "Специализация"); }
 
     public Doctor(int id, string name, string specialization)
     {
